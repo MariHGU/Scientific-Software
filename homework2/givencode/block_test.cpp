56ac44de1c98774a9e4a1db588dd2e5c93b391d5
Mari Gusdal <r1080472@mol.student.cs.kuleuven.be>
2025-11-11 22:34:03 +0100

    block_test

--- homework2/givencode/block_test.cpp
+++ homework2/givencode/block_test.cpp
@@ -6,6 +6,88 @@
 #include "twsmatrix.hpp"
 
 namespace tws{
+
+inline void matmul_naive(const matrixview<> A,
+                         const matrixview<> B,
+                         matrixview<> C,
+                         const double alpha = 1.,
+                         const double beta = 0.)
+{
+    if (beta == 0.) {
+        // Note: we single out beta = 0, because C might be uninitialized and
+        // 0 * Nan = Nan.
+        for (int i = 0; i < A.num_rows(); ++i) {
+            for (int j = 0; j < B.num_columns(); ++j) {
+                C(i, j) = 0.;
+            }
+        }
+    } else {
+        for (int i = 0; i < A.num_rows(); ++i) {
+            for (int j = 0; j < B.num_columns(); ++j) {
+                C(i, j) *= beta;
+            }
+        }
+    }
+
+    // Note: this is a rewritten version of the pure naive implementation:
+    // for (int i = 0; i < A.num_rows(); ++i) {
+    //     for (int j = 0; j < B.num_columns(); ++j) {
+    //         for (int k = 0; k < A.num_columns(); ++k) {
+    //             C(i, j) += alpha * A(i, k) * B(k, j);
+    //         }
+    //     }
+    // }
+    for (int i = 0; i < A.num_rows(); ++i) {
+        for (int j = 0; j < B.num_columns(); ++j) {
+            double sum = 0.;
+            for (int k = 0; k < A.num_columns(); ++k) {
+                sum += alpha * A(i, k) * B(k, j);
+            }
+            C(i, j) += sum;
+        }
+    }
+}
+
+inline void matmul_reordered(const matrixview<> A,
+                             const matrixview<> B,
+                             matrixview<> C,
+                             const double alpha = 1.,
+                             const double beta = 0.)
+{
+    int m = A.num_rows();
+    int n = B.num_columns();
+    int K = A.num_columns();
+
+    if (beta == 0.) {
+        // Note: we single out beta = 0, because C might be uninitialized and
+        // 0 * Nan = Nan.
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                C(i, j) = 0.;
+            }
+        }
+    } else {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                C(i, j) *= beta;
+            }
+        }
+    }
+
+    for (int j = 0; j < n; ++j)
+    {
+        for (int k = 0; k < K; ++k)
+        {
+            auto t = alpha*B(k, j);
+            for (int i=0; i < m; ++i)
+            {
+                C(i, j) += t * A(i, k);
+            }
+        }   
+    }
+}
+
+
 inline void matmul_blocks(const matrixview<> A,
                           const matrixview<> B,
                           matrixview<> C,
@@ -94,51 +176,71 @@ inline void matmul_blocks_b(const matrixview<> A,
             }
         }
     }
-}
 
 
-int main(){
-    int n = 2000;
-    std::cout << "bs  " << "matmul_blocks  " << "matmul_blocks_b" << std::endl;
-
+void time_all(const matrixview<> A,
+                            const matrixview<> B,
+                            matrixview<> C,
+                            const double alpha = 1.,
+                            const double beta = 0.){
     std::list<int> block_sizes = {25, 100, 500};
 
-    double alpha = 1.0;
-    double beta = 0.;
+    auto t0_n =std::chrono::steady_clock::now();
+    matmul_naive(A, B,C, alpha, beta);
+    auto t1_n = std::chrono::steady_clock::now();
 
-    tws::matrix<> A(n,n);
-    tws::matrix<> B(n,n);
-    tws::matrix<> C(n,n);
+    double diff_n = std::chrono::duration<double>(t1_n-t0_n).count();
+    
+    auto t0_r =std::chrono::steady_clock::now();
+    matmul_reordered(A, B,C, alpha, beta);
+    auto t1_r = std::chrono::steady_clock::now();
 
-    randomize(A);
-    randomize(B);
-    randomize(C);
-
-
+    double diff_r = std::chrono::duration<double>(t1_r-t0_r).count();
+    std::cout << "matmul_naive: " << diff_n << std::endl;
+    std::cout << "matmul_recursive: " << diff_r << std::endl;
+    
+    
+    std::cout << "bs  " << "matmul_blocks  " << "matmul_blocks_b" << std::endl;
     for (int block_size : block_sizes)
     {
         double diff=0;
         double diff_b=0;
 
-        for (int i=0; i < 3; ++i)
-        {
-            auto t0 = std::chrono::steady_clock::now();
-            matmul_blocks(A,B, C, alpha, beta, block_size);
-            auto t1 = std::chrono::steady_clock::now();
-            diff += std::chrono::duration<double>(t1-t0).count();            
+        auto t0 = std::chrono::steady_clock::now();
+        matmul_blocks(A,B, C, alpha, beta, block_size);
+        auto t1 = std::chrono::steady_clock::now();
+        diff += std::chrono::duration<double>(t1-t0).count();            
 
-            auto t0_b = std::chrono::steady_clock::now();
-            matmul_blocks_b(A,B, C, alpha, beta, block_size);
-            auto t1_b = std::chrono::steady_clock::now();
-            diff_b += std::chrono::duration<double>(t1_b-t0_b).count();            
-        }
+        auto t0_b = std::chrono::steady_clock::now();
+        matmul_blocks_b(A,B, C, alpha, beta, block_size);
+        auto t1_b = std::chrono::steady_clock::now();
+        diff_b += std::chrono::duration<double>(t1_b-t0_b).count();  
+        
 
-        double diff_avg = diff/3;
-        double diff_b_avg = diff_b/3;
+        double diff_avg = diff;
+        double diff_b_avg = diff_b;
 
         std::cout << block_size << " " << diff_avg << " " << diff_b_avg << std::endl;
     }
+}
+}
+
+int main(){
+    int n = 2000;
+        
+    double alpha = 1.0;
+    double beta = 0.;
     
-    return 1;
+    tws::matrix<> A(n,n);
+    tws::matrix<> B(n,n);
+    tws::matrix<> C(n,n);
+    
+    randomize(A);
+    randomize(B);
+    randomize(C);
+    
+    time_all(A,B,C,alpha,beta);
+    
+    return 0;
 
 }
