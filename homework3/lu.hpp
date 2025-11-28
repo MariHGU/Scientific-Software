3b16ec08f130d2e342b5b7cdd7baa7e387097e3b
Mari <134164706+MariHGU@users.noreply.github.com>
2025-11-28 14:02:54 +0100

    fremgang ish

--- homework3/lu.hpp
+++ homework3/lu.hpp
@@ -134,59 +134,57 @@ void lu_v2(matrixview<T> A, vectorview<int> ipiv)
 template <typename T>
 void lu_v3(matrixview<T> A, vectorview<int> ipiv)
 {
-    assert(ipiv.size() == std::min(A.num_rows(), A.num_columns()));
+    int min_val = std::min(A.num_rows(), A.num_columns());
+    assert(ipiv.size() == min_val);
 
-    // Combine trsm_ll_v2 to implement blocked version of LU factorisation
     int m = A.num_rows();
     int n = A.num_columns();
 
-    // if block is small enough -> skip blocking and just solve normally
-    // maybe test different numbers here too
-    if (m < 16){
-        lu_v2(A, ipiv);
-        return;
-    }
-
     int nb = 128; // block size
-    for (int i =0; i < n; i += nb){
+    for (int i =0; i < min_val; i += nb){
         //find width:
-        int bi = std::min(nb, std::min(n, m) - i);
+        int bi = std::min(nb, min_val - i);
 
         // .submatrix(start row, end row, start col, end col)
-        auto A00 = A.submatrix(i, m, i, bi+i);
-        auto ipiv_panel = ipiv.subvector(i, m);
-        lu_v2(A00, ipiv_panel); // -> Now A00 = L00 and U00?
+        auto A00_10 = A.submatrix(i, m, i, bi+i);
+        auto ipiv_panel = ipiv.subvector(i, bi+i);
+        lu_v2(A00_10, ipiv_panel); // -> Now A00 = L00 and U00 and A10 = L10 and U10
 
-        auto A01 = A.submatrix(i, m, bi+i, n);
-        auto A10 = A.submatrix(bi+i, m, i, bi+i);
-        auto A11 = A.submatrix(bi+i, m, bi+i, n);
         
         // apply pivoting:
-        // A01 = ipiv_panel * A01;
-        // A10 = ipiv_panel * A10;
-        // A11 = ipiv_panel * A11;
-
         //pivot index runs from 0 to bi-1
-        // ipiv[i]=j => row i swapped with row j
-        for (int p = 0; p < bi; ++i)
+        for (int p = 0; p < bi; ++p)
         {
             int global_p = p + i; // global meaning complete A matrix
             int global_q = i + ipiv_panel[p]; // row that was swapped
-
-            // Apply swap globally
-            for (int col = 0; col < A10.num_columns(); ++col){
-                std::swap(A10(global_p, col), A10(global_q, col));
+            ipiv[global_p] = global_q; // store global pivot index
+            
+            if (global_p == global_q){
+                continue; // no swap needed
+            }
+            
+            // Apply swap globally, but only from columns to the right of A00
+            for (int col = bi+i; col < n; ++col){
+                std::swap(A(global_p, col), A(global_q, col));
             }
 
         }
         
-
-        trsm_ll_v2(A00, A01); // U01 is now in A01
-
-        // L10 = A10*U00^-1
-        // => A10 <- A10 - L10*U00
-        tws::matmul_blocked_a(L10, A00, A10, T(-1), T(0.0))
-        lu_v3(A11, ipiv); // recursive call
+        if(bi + i >= n){
+            continue;
+        }
+        // retrieve pivoted submatricies
+        auto A01 = A.submatrix(i, m, bi+i, n);
+        auto A10 = A.submatrix(bi+i, m, i, bi+i); // L10 is in A10
+        auto A11 = A.submatrix(bi+i, m, bi+i, n);
+        
+        // U01 = L00^-1 * A01
+        auto L00 = A.submatrix(i, bi+i, i, bi+i); // L00 is the bi x bi square in A00
+        auto U01 = A.submatrix(i, bi+i, i+bi, n); // Top bi rows of A01
+        trsm_ll_v2(L00, U01); // Solution stored in U01
+        
+        // => A11 <- A11 - L10*U01
+        tws::matmul_blocked_a(A10, U01, A11, T(-1), T(1));
     }
 }
 
@@ -200,6 +198,19 @@ template <typename T>
 void lu_v4(matrixview<T> A, vectorview<int> ipiv)
 {
     // TODO: implement this function
+    int min_val = std::min(A.num_rows(), A.num_columns());
+    assert(ipiv.size() == min_val);
+
+    int m = A.num_rows();
+    int n = A.num_cokumns();
+
+    int nb = 128;
+    // iterate through blocks
+    for (int i = 0; i < min_val; i+=nb){
+
+    }
+
+
 }
 
 /**
