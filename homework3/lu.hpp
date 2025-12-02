e51dc09c0ad1b4b74865ce4ede5edc0cb0a2f139
Mari <134164706+MariHGU@users.noreply.github.com>
2025-12-02 12:08:20 +0100

    small fix

--- homework3/lu.hpp
+++ homework3/lu.hpp
@@ -154,8 +154,7 @@ void lu_v3(matrixview<T> A, vectorview<int> ipiv)
     int m = A.num_rows();
     int n = A.num_columns();
 
-    //int nb = 128; // block size
-    int nb = 256;
+    int nb = 128; // block size
     for (int i =0; i < min_val; i += nb){
         //find width:
         int bi = std::min(nb, min_val - i);
