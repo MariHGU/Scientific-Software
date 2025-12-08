f80515cd0e31a89dca0a2bdf474f63a7135b6e34
Mari <134164706+MariHGU@users.noreply.github.com>
2025-12-08 10:11:07 +0100

    added fortran

---
+++ practice/session6/main_f.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "fortran_add.h"
+#include "twsmatrix.hpp"
+
+
+using namespace tws;
+
+int main(){
+    vector<double> A(3);
+    vector<double> B(3);
+
+    randomize(A);
+    randomize(B);
+
+    print_vector(A);
+    print_vector(B);
+
+    c_add(3, A.data(), B.data());
+
+    print_vector(B);
+
+    return 0;
+
+}
