e5fc57c4266995a8473099d51a7118b8c146b622
Mari <134164706+MariHGU@users.noreply.github.com>
2025-12-10 15:13:01 +0100

    lapack

--- practice/sesh6/c_lapack.h
+++ practice/sesh6/c_lapack.h
@@ -5,8 +5,41 @@
 extern "C"{
 #endif
 
-void daxpy_(const int* n, const double* alpha, const double* x, const int* stridex, double* y, const int* stridey);
+// BLAS
+void daxpy_(const int* n,
+            const double* alpha,
+            const double* x, const int* incx,
+            double* y, const int* incy);
 
+// LAPACK
+void dgeqrf_(const int* m, const int* n,
+             double* A, const int* lda,
+             double* tau,
+             double* work, const int* lwork,
+             int* info);
+
+void dormqr_(const char* side, const char* trans,
+             const int* m, const int* n, const int* k,
+             const double* A, const int* lda,
+             const double* tau,
+             double* C, const int* ldc,
+             double* work, const int* lwork,
+             int* info);
+
+void dtrsm_(const char* side, const char* uplo,
+            const char* transa, const char* diag,
+            const int* m, const int* n,
+            const double* alpha,
+            const double* A, const int* lda,
+            double* B, const int* ldb);
+
+void dgemv_(const char* trans,
+            const int* m, const int* n,
+            const double* alpha,
+            const double* A, const int* lda,
+            const double* x, const int* incx,
+            const double* beta,
+            double* y, const int* incy);
 
 #ifdef __cplusplus
 }
