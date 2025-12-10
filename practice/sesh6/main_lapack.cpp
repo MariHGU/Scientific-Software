5d91fe602af5248433b6664a658d113c5e8efd70
Mari <134164706+MariHGU@users.noreply.github.com>
2025-12-10 13:33:22 +0100

    lapack

--- practice/sesh6/main_lapack.cpp
+++ practice/sesh6/main_lapack.cpp
@@ -8,7 +8,7 @@ using namespace tws;
 void call_daxpy(vector<double>& a, vector<double>& b, double& alpha){
     assert(a.size() == b.size());
 
-    int n = a.size()
+    int n = a.size();
 
     daxpy_(&n, &alpha, a.data(), 1, b.data(), 1);
 }
