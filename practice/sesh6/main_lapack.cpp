c29cc8c07f23f9e65b882385accd0170036a5c8b
Mari <134164706+MariHGU@users.noreply.github.com>
2025-12-10 15:05:21 +0100

    lapack

--- practice/sesh6/main_lapack.cpp
+++ practice/sesh6/main_lapack.cpp
@@ -15,21 +15,54 @@ void call_daxpy(vector<double>& a, vector<double>& b, double& alpha){
 }
 
 int main(){
-    double alpha = 3.0;
+    double alpha = 1.0;
     int n =3;
 
+    matrix<double> A(n,n);
     vector<double> x(n);
     vector<double> y(n);
 
+    randomize(A);
     randomize(x);
     randomize(y);
 
-    print_vector(x);
+    matrix<double> A_orig = A;
+
+    //print_vector(x);
     print_vector(y);
 
-    call_daxpy(x, y, alpha);
+    int ldim = A.ldim();
+    int info;
+    double wkopt;
+    int lwork=-1;
+    char side = 'L';
+    char T = 'T';
+    char N = 'N';
+    char U = 'U';
+    int inc = 1;
+    //call_daxpy(x, y, alpha);
+    
+    vector<double> tau(n);
+    dgeqrf_(&n, &n, A.data(), &ldim, tau.data(), &wkopt, &lwork, &info);
 
-    print_vector(y);
+    lwork = (int)wkopt;
+    vector<double> work(lwork);
+    dgeqrf_(&n, &n, A.data(), &ldim, tau.data(), work.data(), &lwork, &info);
+
+    // compute y=Qt b
+    dormqr_(&side, &T, &n, &inc, &n, A.data(), &ldim, tau.data(), y.data(), &n, work.data(), &lwork, &info);
+
+    // solve Rx = y
+    dtrsm_(&side, &U, &N, &N, &n, &inc, &alpha, A.data(), &ldim, y.data(), &n);
+
+    // Check solution:
+    double beta =0.0;
+    vector<double> Ax(n);
+
+    dgemv_(&N, &n, &n, &alpha, A_orig.data(), &ldim, y.data(), &inc, &beta, Ax.data(), &inc);
+    //print_vector(y);
+
+    print_vector(Ax);
 
     return 0;
 }
